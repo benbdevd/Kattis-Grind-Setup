7e3f7965471d0195dfa505867cbd8a4541dfe0bb
Benjamin J. Boyle <me@benboyle.ca>
2019-09-27 20:46:36 -0300

    python for getting all problem ids will now fill folders for all problem ids

--- get-all-problem-ids.py
+++ get-all-problem-ids.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
+import os
 import requests
 import threading
+
+OUTPUT_FILE = 'allProblemIDs.txt'
 
 def get_ids(page_num):
      global all_ids
@@ -10,21 +13,29 @@ def get_ids(page_num):
      for link in soup.findAll('a', href=True):
           if "/problems/" in link['href'] and link['href'].count("/") == 2:
                all_ids.append(link['href'].split("/")[2])
-     
-     
-all_ids = []
-all_threads = []
-for i in range(25):
-     print("Starting page: ", i)
-     t = threading.Thread(target=get_ids, args=(i,))
-     t.start()
-     all_threads.append(t)
-     
-for thread in all_threads:
-     thread.join()
 
-print("All pages read.")
-all_ids.sort()
-with open('allProblemIDs.txt', 'w') as f:
-    for item in all_ids:
-        f.write("%s\n" % item)
+
+if not os.path.exists(OUTPUT_FILE):
+     all_ids = []
+     all_threads = []
+     for i in range(25):
+          print("Starting page: ", i)
+          t = threading.Thread(target=get_ids, args=(i,))
+          t.start()
+          all_threads.append(t)
+          
+     for thread in all_threads:
+          thread.join()
+
+     print("All pages read.")
+     all_ids.sort()
+     with open(OUTPUT_FILE, 'w') as f:
+          for item in all_ids:
+               f.write("%s\n" % item)
+
+with open(OUTPUT_FILE) as problem_list:
+     for problem_id in problem_list:
+          if os.name is 'nt':
+               os.system('new-problem.bat ' + problem_id)
+          else:
+               os.system('./newprob.sh ' + problem_id)
